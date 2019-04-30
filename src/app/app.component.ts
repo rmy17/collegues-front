6b8a9dd663b255fb19be8f680f8a2c7e0400eab9
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-04-30 16:17:51 +0200

    commit correction mise en page 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,28 @@ import { collegueMock } from './mock/collegues.mock';
   selector: 'app-root',
   template: `
 
-    <app-collegue [col]="unObjetCollegueFourni"></app-collegue>
-    <app-recherche-collegue-par-nom><app-recherche-collegue-par-nom
+  <div class="container-fluid">
+  <div class="row justify-content-center">
+  <h1>Admission collegue</h1>
+  </div>
+  </div>
+
+  <div class="container-fluid">
+  <div class="row justify-content-center">
+  <div class="col-6">
+  <app-recherche-collegue-par-nom></app-recherche-collegue-par-nom>
+  </div>
+  <div class="col-6">
+  <app-collegue [col]="unObjetCollegueFourni"></app-collegue>
+  </div>
+</div>
+</div>
+
+    
   `,
   styles: []
 })
 export class AppComponent {
   title = 'collegues-front';
-  unObjetCollegueFourni  = collegueMock;
+  unObjetCollegueFourni = collegueMock;
 }
