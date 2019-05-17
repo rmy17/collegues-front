40be51cac146199868071475eb0b871ee46a2f8d
rmy17 <remyvi17@gmail.com>
2019-05-17 19:57:25 +0200

    correction erreur url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlRecupNom : "https://remvia-collegues-api.herokuapp.com/collegues",
-  urlServ : "https://remvia-collegues-api.herokuapp.com/auth"
+  urlServ : "https://remvia-collegues-api.herokuapp.com/"
 };
